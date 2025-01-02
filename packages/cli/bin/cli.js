05e74498cd953db22b6c638c7c15ac538afec13f
LxHouse <364403299@qq.com>
2025-01-02 16:40:30 +0800

    feat: 改造为es

--- packages/cli/bin/cli.js
+++ packages/cli/bin/cli.js
@@ -1,7 +1,10 @@
 #! /usr/bin/env node
-const importLocal = require('import-local');
-const { log } = require('@zmcli/utils');
-const entry = require('../lib/index');
+import importLocal from 'import-local';
+import { log } from '@zmcli/utils';
+import entry from '../lib/index.js';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(import.meta.url);
 if (importLocal(__dirname)) {
   log.info('cli', '本次使用的版本！');
 } else {
--- packages/cli/lib/index.js
+++ packages/cli/lib/index.js
@@ -1,21 +1,29 @@
-const command = require('commander');
-const pkg = require('../package.json');
-const initCreate = require('@zmcli/init');
-const semvar = require('semver');
-const { log, isDebug } = require('@zmcli/utils');
-const { program } = command;
+import { program } from 'commander';
+import path from 'node:path';
+import fse from 'fs-extra';
+import initCreate from '@zmcli/init';
+import chalk from 'chalk';
+import semvar from 'semver';
+import { log, isDebug } from '@zmcli/utils';
+import { fileURLToPath } from 'node:url';
 
-const LOW_NODE_VERSION = '22.0.0';
+const LOW_NODE_VERSION = '14.0.0';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__filename, __dirname);
+const pkgPath = path.resolve(__dirname, '../package.json');
+const pkg = fse.readJsonSync(pkgPath);
+
 function checkNodeVersion() {
   log.verbose('node version', process.version);
   if (semvar.lt(process.version, LOW_NODE_VERSION)) {
-    throw new Error(`需要安装 ${LOW_NODE_VERSION} 以上版本的Node.js`);
+    throw new Error(chalk.red('需要安装 22.0.0 以上版本的Node.js'));
   }
 }
 function preAction() {
   checkNodeVersion();
 }
-module.exports = function (args) {
+export default function (args) {
   log.info(pkg.version);
   program
     .name(Object.getOwnPropertyNames(pkg.bin)[0] || '')
@@ -26,7 +34,7 @@ module.exports = function (args) {
 
   initCreate(program);
   program.parse(process.argv);
-};
+}
 
 process.on('uncaughtException', (e) => {
   console.log(isDebug());
